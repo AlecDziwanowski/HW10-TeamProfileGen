13dd81e0eeb09f2674cefd3edd4e9aa69216c260
Alec Dziwanowski <adziwanowski@gmail.com>
2022-04-26 00:49:45 -0400

    continued troubleshooting html writing

--- index.js
+++ index.js
@@ -127,21 +127,61 @@ function nextAction() {
             } else if (action === 'Intern') {
                 internPrompts();
             } else {
-                process.exit();
                 writeHTML(employees);
+                process.exit();
             }
         });
 }
 
 function writeHTML(employees) {
     // Write the following text to a new html file
-    console.log(employees);
-    fs.writeFile('/dist/team.html', 
-    ``
-    
-    , err =>
-        err ? console.error(err) : console.log('Generating Web page...'))
+    console.log(employees[0].name);
+    fs.writeFile('dist/team.html', `
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>[Team HasselHoff]</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center bg-danger text-white p-5">[Team HasselHoff]</h1>
+    </header>
+
+    <main class="d-flex justify-content-center">
+    ${generateCards(employees)}
+    </main>
+</body>
+
+</html>
+`
+        , err =>
+            err ? console.error(err) : console.log('Generating Web page...'))
+}
+
+function generateCards(employees) {
+    return `
+<section class="card border rounded m-3 customShadow">
+    <div class="bg-primary card-body text-white border rounded-top">
+        <h2 class="card-title">${employees[0].name}</h2>
+        <h3 class="card-title">[👔 Manager]</h3>
+    </div>
+    <div class="bg-light border rounded-bot">
+        <ul class="list-group m-4">
+            <li class="list-group-item">ID: ${employees[0].id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${employees[0].email}">${employees[0].email}</a></li>
+            <li class="list-group-item">[Office Number: [240]]</li>
+        </ul>
+    </div>
+</section>
+`;
 }
 
 
